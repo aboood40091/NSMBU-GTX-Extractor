45215df2ff83c894bbffc95aebc197d3daba6983
AboodXD <aboood40091@gmail.com>
2017-06-23 18:40:05 +0400

    Remove DXT5 - 1

--- gtx_extract.c
+++ gtx_extract.c
@@ -12,8 +12,7 @@
  * How to build:
  * gcc -m64 -o gtx_extract gtx_extract.c
  *
- * This tool currently supports RGBA8 (format 0x1A) and DXT5 (format 0x33)
- * textures.
+ * This tool currently supports RGBA8 (format 0x1A) textures.
  * The former is known to work with 2048x512 textures.
  * The latter has been tested successfully with 512x320 and 2048x512 textures,
  * and is known to be broken with 384x256 textures.
@@ -30,178 +29,12 @@
  * Feel free to throw a pull request at me if you improve it!
  */
 
-#include "txc_dxtn.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <setjmp.h>
 
-/* Start of libtxc_dxtn section */
-#define EXP5TO8R(packedcol)					\
-   ((((packedcol) >> 8) & 0xf8) | (((packedcol) >> 13) & 0x7))
-
-#define EXP6TO8G(packedcol)					\
-   ((((packedcol) >> 3) & 0xfc) | (((packedcol) >>  9) & 0x3))
-
-#define EXP5TO8B(packedcol)					\
-   ((((packedcol) << 3) & 0xf8) | (((packedcol) >>  2) & 0x7))
-
-#define EXP4TO8(col)						\
-   ((col) | ((col) << 4))
-
-/* inefficient. To be efficient, it would be necessary to decode 16 pixels at once */
-
-static void dxt135_decode_imageblock ( const GLubyte *img_block_src,
-                         GLint i, GLint j, GLuint dxt_type, GLvoid *texel ) {
-   GLchan *rgba = (GLchan *) texel;
-   const GLushort color0 = img_block_src[0] | (img_block_src[1] << 8);
-   const GLushort color1 = img_block_src[2] | (img_block_src[3] << 8);
-   const GLuint bits = img_block_src[4] | (img_block_src[5] << 8) |
-      (img_block_src[6] << 16) | (img_block_src[7] << 24);
-   /* What about big/little endian? */
-   GLubyte bit_pos = 2 * (j * 4 + i) ;
-   GLubyte code = (GLubyte) ((bits >> bit_pos) & 3);
-
-   rgba[ACOMP] = CHAN_MAX;
-   switch (code) {
-   case 0:
-      rgba[RCOMP] = UBYTE_TO_CHAN( EXP5TO8R(color0) );
-      rgba[GCOMP] = UBYTE_TO_CHAN( EXP6TO8G(color0) );
-      rgba[BCOMP] = UBYTE_TO_CHAN( EXP5TO8B(color0) );
-      break;
-   case 1:
-      rgba[RCOMP] = UBYTE_TO_CHAN( EXP5TO8R(color1) );
-      rgba[GCOMP] = UBYTE_TO_CHAN( EXP6TO8G(color1) );
-      rgba[BCOMP] = UBYTE_TO_CHAN( EXP5TO8B(color1) );
-      break;
-   case 2:
-      if (color0 > color1) {
-         rgba[RCOMP] = UBYTE_TO_CHAN( ((EXP5TO8R(color0) * 2 + EXP5TO8R(color1)) / 3) );
-         rgba[GCOMP] = UBYTE_TO_CHAN( ((EXP6TO8G(color0) * 2 + EXP6TO8G(color1)) / 3) );
-         rgba[BCOMP] = UBYTE_TO_CHAN( ((EXP5TO8B(color0) * 2 + EXP5TO8B(color1)) / 3) );
-      }
-      else {
-         rgba[RCOMP] = UBYTE_TO_CHAN( ((EXP5TO8R(color0) + EXP5TO8R(color1)) / 2) );
-         rgba[GCOMP] = UBYTE_TO_CHAN( ((EXP6TO8G(color0) + EXP6TO8G(color1)) / 2) );
-         rgba[BCOMP] = UBYTE_TO_CHAN( ((EXP5TO8B(color0) + EXP5TO8B(color1)) / 2) );
-      }
-      break;
-   case 3:
-      if ((dxt_type > 1) || (color0 > color1)) {
-         rgba[RCOMP] = UBYTE_TO_CHAN( ((EXP5TO8R(color0) + EXP5TO8R(color1) * 2) / 3) );
-         rgba[GCOMP] = UBYTE_TO_CHAN( ((EXP6TO8G(color0) + EXP6TO8G(color1) * 2) / 3) );
-         rgba[BCOMP] = UBYTE_TO_CHAN( ((EXP5TO8B(color0) + EXP5TO8B(color1) * 2) / 3) );
-      }
-      else {
-         rgba[RCOMP] = 0;
-         rgba[GCOMP] = 0;
-         rgba[BCOMP] = 0;
-         if (dxt_type == 1) rgba[ACOMP] = UBYTE_TO_CHAN(0);
-      }
-      break;
-   default:
-   /* CANNOT happen (I hope) */
-      break;
-   }
-}
-
-
-void fetch_2d_texel_rgba_dxt5(GLint srcRowStride, const GLubyte *pixdata,
-                         GLint i, GLint j, GLvoid *texel) {
-
-   /* Extract the (i,j) pixel from pixdata and return it
-    * in texel[RCOMP], texel[GCOMP], texel[BCOMP], texel[ACOMP].
-    */
-
-   GLchan *rgba = (GLchan *) texel;
-   const GLubyte *blksrc = (pixdata + ((srcRowStride + 3) / 4 * (j / 4) + (i / 4)) * 16);
-   const GLubyte alpha0 = blksrc[0];
-   const GLubyte alpha1 = blksrc[1];
-#if 0
-   const GLubyte bit_pos = 3 * ((j&3) * 4 + (i&3));
-   /* simple 32bit version */
-   const GLuint bits_low = blksrc[2] | (blksrc[3] << 8) | (blksrc[4] << 16) | (blksrc[5] << 24);
-   const GLuint bits_high = blksrc[6] | (blksrc[7] << 8);
-   GLubyte code;
-
-   if (bit_pos < 30)
-      code = (GLubyte) ((bits_low >> bit_pos) & 7);
-   else if (bit_pos == 30)
-      code = (GLubyte) ((bits_low >> 30) & 3) | ((bits_high << 2) & 4);
-   else
-      code = (GLubyte) ((bits_high >> (bit_pos - 32)) & 7);
-#endif
-#if 1
-/* TODO test this! */
-   const GLubyte bit_pos = ((j&3) * 4 + (i&3)) * 3;
-   const GLubyte acodelow = blksrc[2 + bit_pos / 8];
-   const GLubyte acodehigh = blksrc[3 + bit_pos / 8];
-   const GLubyte code = (acodelow >> (bit_pos & 0x7) |
-      (acodehigh  << (8 - (bit_pos & 0x7)))) & 0x7;
-#endif
-   dxt135_decode_imageblock(blksrc + 8, (i&3), (j&3), 2, texel);
-#if 0
-   if (alpha0 > alpha1) {
-      switch (code) {
-      case 0:
-         rgba[ACOMP] = UBYTE_TO_CHAN( alpha0 );
-         break;
-      case 1:
-         rgba[ACOMP] = UBYTE_TO_CHAN( alpha1 );
-         break;
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-         rgba[ACOMP] = UBYTE_TO_CHAN( ((alpha0 * (8 - code) + (alpha1 * (code - 1))) / 7) );
-         break;
-      }
-   }
-   else {
-      switch (code) {
-      case 0:
-         rgba[ACOMP] = UBYTE_TO_CHAN( alpha0 );
-         break;
-      case 1:
-         rgba[ACOMP] = UBYTE_TO_CHAN( alpha1 );
-         break;
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-         rgba[ACOMP] = UBYTE_TO_CHAN( ((alpha0 * (6 - code) + (alpha1 * (code - 1))) / 5) );
-         break;
-      case 6:
-         rgba[ACOMP] = 0;
-         break;
-      case 7:
-         rgba[ACOMP] = CHAN_MAX;
-         break;
-      }
-   }
-#endif
-/* not sure. Which version is faster? */
-#if 1
-/* TODO test this */
-   if (code == 0)
-      rgba[ACOMP] = UBYTE_TO_CHAN( alpha0 );
-   else if (code == 1)
-      rgba[ACOMP] = UBYTE_TO_CHAN( alpha1 );
-   else if (alpha0 > alpha1)
-      rgba[ACOMP] = UBYTE_TO_CHAN( ((alpha0 * (8 - code) + (alpha1 * (code - 1))) / 7) );
-   else if (code < 6)
-      rgba[ACOMP] = UBYTE_TO_CHAN( ((alpha0 * (6 - code) + (alpha1 * (code - 1))) / 5) );
-   else if (code == 6)
-      rgba[ACOMP] = 0;
-   else
-      rgba[ACOMP] = CHAN_MAX;
-#endif
-}
-
-/* Start of GTX Extractor section */
 typedef struct _GTXData {
 	uint32_t width, height;
 	uint32_t format;
@@ -374,54 +207,6 @@ void export_RGBA8(GTXData *gtx, FILE *f) {
 	free(output);
 }
 
-void export_DXT5(GTXData *gtx, FILE *f) {
-	uint32_t pos, x, y;
-	uint32_t *output, outValue;
-	uint32_t blobWidth = gtx->width / 4, blobHeight = gtx->height / 4;
-	uint8_t bits[4];
-	__uint128_t *src = (__uint128_t *)gtx->data;
-	__uint128_t *work = malloc(gtx->width * gtx->height);
-
-	for (y = 0; y < blobHeight; y++) {
-		for (x = 0; x < blobWidth; x++) {
-			pos = (y >> 4) * (blobWidth * 16);
-			pos ^= (y & 1);
-			pos ^= (x & 7) << 1;
-			pos ^= (x & 8) << 1;
-			pos ^= (x & 8) << 2;
-			pos ^= (x & 0x10) << 2;
-			pos ^= (x & ~0x1F) << 4;
-			pos ^= (y & 2) << 6;
-			pos ^= (y & 4) << 6;
-			pos ^= (y & 8) << 1;
-			pos ^= (y & 0x10) << 2;
-			pos ^= (y & 0x20);
-
-			work[(y*blobWidth)+x] = src[pos];
-		}
-	}
-
-	output = malloc(gtx->width * gtx->height * 4);
-
-	for (y = 0; y < gtx->height; y++) {
-		for (x = 0; x < gtx->width; x++) {
-			fetch_2d_texel_rgba_dxt5(gtx->width, (uint8_t *)work, x, y, bits);
-
-			outValue = (bits[ACOMP] << 24);
-			outValue |= (bits[RCOMP] << 16);
-			outValue |= (bits[GCOMP] << 8);
-			outValue |= bits[BCOMP];
-
-			output[(y * gtx->width) + x] = outValue;
-		}
-	}
-
-	writeFile(f, gtx->width, gtx->height, (uint8_t *)output);
-
-	free(output);
-	free(work);
-}
-
 int main(int argc, char **argv) {
 	GTXData data;
 	FILE *f;
@@ -457,8 +242,6 @@ int main(int argc, char **argv) {
 
 	if (data.format == 0x1A)
 		export_RGBA8(&data, f);
-	else if (data.format == 0x33)
-		export_DXT5(&data, f);
 	fclose(f);
 
 	return EXIT_SUCCESS;
